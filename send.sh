82176fe4336ca578cc5c70a94c0fe47c6c27e71b
doristeo <tengizi@gmail.com>
2021-03-07 21:40:49 +0300

    Чеки тоже выцепляем

--- send.sh
+++ send.sh
@@ -5,7 +5,7 @@ function makejson(){
   peers=$(curl -s http://localhost:1635/peers | jq '.peers | length')
   diskavail=$(df -P . | awk 'NR==2{print $2}')
   diskfree=$(df -P . | awk 'NR==2{print $4}')
-  json='{"name":"'"$name"'","peers":'$peers',"diskavail":'$diskavail',"diskfree":'$diskfree'}'
+  json='{"name":"'"$name"'","peers":'$peers',"diskavail":'$diskavail',"diskfree":'$diskfree',"cheque":'$cheque'}'
 }
 
 if [ $# -eq 0 ]
