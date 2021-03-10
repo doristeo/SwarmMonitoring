ae3432ece8ff98386e987a099fb1080a5406b5c7
doristeo <tengizi@gmail.com>
2021-03-10 15:23:10 +0300

    Update monsrvinst.sh

--- monsrvinst.sh
+++ monsrvinst.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
 
+# install docker & compose
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# download files
+sudo su
+cd /root
+wget https://raw.githubusercontent.com/doristeo/SwarmMonitoring/main/docker-compose.yaml
+mkdir /init
+wget https://raw.githubusercontent.com/doristeo/SwarmMonitoring/main/createdb.sql -P /root/init
+
