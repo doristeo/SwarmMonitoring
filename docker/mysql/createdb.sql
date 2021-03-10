118d0715ebf90e807e81c4bbc1394980a17e318a
doristeo <tengizi@gmail.com>
2021-03-10 21:41:25 +0300

    Update createdb.sql

--- docker/mysql/createdb.sql
+++ docker/mysql/createdb.sql
@@ -1,1 +1,11 @@
-
+CREATE DATABASE swarms;
+USE swarms;
+CREATE TABLE `nodes` (
+  `timeof` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(60) NOT NULL,
+  `peers` int NOT NULL,
+  `diskavail` bigint DEFAULT NULL,
+  `diskfree` bigint DEFAULT NULL,
+  `cheque` int NOT NULL,
+  KEY `index1` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
