416d4d5ddb0eaabb3651475f060bba8f91bb19d6
doristeo <tengizi@gmail.com>
2021-03-10 21:55:35 +0300

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -10,7 +10,10 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo wget https://raw.githubusercontent.com/doristeo/SwarmMonitoring/main/docker-compose.yaml
-sudo mkdir /root/init
-sudo wget https://raw.githubusercontent.com/doristeo/SwarmMonitoring/main/createdb.sql -P /root/init
+sudo wget https://raw.githubusercontent.com/doristeo/SwarmMonitoring/main/docker/docker-compose.yaml
+sudo mkdir /root/swarmon/ && sudo mkdir /root/swarmon/mysql && sudo mkdir /root/swarmon/mysql/init
+sudo wget https://raw.githubusercontent.com/doristeo/SwarmMonitoring/main/docker/mysql/createdb.sql -P /root/swarmon/mysql/init
+sudo mkdir /root/swarmon/grafana
+sudo wget https://github.com/doristeo/SwarmMonitoring/blob/c3c25a204d317a043e9ddc804d1cf37fd92ad867/docker/grafana/grafana.tar.gz
+sudo tar -zxvf grafana.tar.gz -C /root/swarmon/grafana
 sudo docker-compose up -d
